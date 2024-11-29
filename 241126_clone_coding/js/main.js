86c8a854abc6a149fd7f4eff5d058c16d43005ed
o3ouo <o3ouo@daum.net>
2024-11-29 15:50:37 +0900

    d

--- 241126_clone_coding/js/main.js
+++ 241126_clone_coding/js/main.js
@@ -1,33 +1,64 @@
+let navBg = document.querySelector(".header .nav-bg");
+let logo = document.querySelector(".header .logo");
+let logoBox = document.querySelector(".header .logo-box");
+
+window.addEventListener("scroll", function () {
+
+  const isScrolled = window.scrollY > 0;
+
+  navBg.classList.toggle("scrolled", isScrolled);
+  logo.classList.toggle("scrolled", isScrolled);
+  logoBox.classList.toggle("scrolled", isScrolled);
+
+  // if (this.window.scrollY > 0) {
+  //   navBg.style.opacity = "1";
+
+  //   logo.style.top = "0";
+  //   logo.style.alignItems = "center";
+    
+  //   logoBox.style.transform = "scale(0.2)";
+  // } else {
+  //   navBg.style.opacity = "0";
+
+  //   logo.style.top = "-6px";
+  //   logo.style.alignItems = "baseline";
+
+  //   logoBox.style.transform = "scale(1)";
+  // };
+
+});
+
 let item = [
   {
     id: 1,
-    img: "./img/sction01_01.jpg",
+    img: "./img/section01_01.jpg",
     name: "KOREAN BBQ",
     info: "Grass-Fed Beef"
   },
   {
     id: 2,
-    img: "./img/sction01_01.jpg",
+    img: "./img/section01_02.jpg",
     name: "LAO CURRY",
     info: "Grass-Fed Beef"
   },
   { id: 3,
-    img: "./img/sction01_01.jpg",
+    img: "./img/section01_03.jpg",
     name: "KALE CHIMICHURRI",
     info: "Chicken Sausage"
   },
   { id: 4,
-    img: "./img/sction01_01.jpg",
+    img: "./img/section01_04.jpg",
     name: "SMOKED CAULI BRAT",
     info: "Chicken Sausage"
   },
   { id: 5,
-    img: "./img/sction01_01.jpg",
+    img: "./img/section01_05.webp",
     name: "MAPLE APPLE",
     info: "Chicken Sausage"
   },
 ];
 
+// item slider에 객체 배열로 만들어둔 item 넣기
 let itemSlider = document.querySelector(".item-slider");
 let slideBtn = document.querySelectorAll(".slide-btn .arrow");
 
@@ -53,7 +84,7 @@ function slide(item) {
   tem = tem.join("");
   console.log(tem);
 
-  itemSlider.innerHTML = item;
+  itemSlider.innerHTML = tem;
 }
 
 slide(item);
